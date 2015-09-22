e58b8e7b0b436a7ef9908b187a26a4ffc0c430ae
Jose Carvajal <joe_carvajal@hotmail.com>
2015-09-22 00:02:00 -0500

    Quiz 1
    Programa en Python

---
+++ laboratorios/quiz1.py
@@ -0,0 +1,20 @@
+#QUIZ no.1 PROGRAMACION III
+
+base_cm = 5
+altura_cm = 7
+
+area_cm = base_cm * altura_cm
+perimetro_cm = base_cm*2 + altura_cm*2
+
+print("El area del rectangulo es : " +str(area_cm) +" cm**2")
+print("El perimetro del rectangulo es: " +str(perimetro_cm) +" cm")
+
+area_m = area_cm/100
+perimetro_m = perimetro_cm/100
+
+area_in = area_cm/2.54
+perimetro_in = perimetro_cm/6.4516
+
+print("El area es: " +str(area_m) +" m**2 y el perimetro " +str(perimetro_m)+ "m")
+
+print("El area es: " +str(area_m) +" in**2 y el perimetro " +str(perimetro_m)+ "in")
