f475eab6467129b3ca778964632e91bfa56e63a9
Jose Carvajal <joe_carvajal@hotmail.com>
2015-09-22 06:50:41 -0500

    Tarea 2 en Python

---
+++ tareas/Tarea2.py
@@ -0,0 +1,10 @@
+#Tarea 2 - Prog III
+
+v = 120
+#velocidad en km/h
+x = 60
+#distancia en km
+t = x/v * 3600
+#tiempo en segundos
+ 
+print("Una ambulancia viaja a " +str(v) +" km/h y recorre " + str(x) +" km en un tiempo de " +str(t) +" s.")
