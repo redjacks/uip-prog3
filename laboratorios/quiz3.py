33ed88403a4f5037c7e2117671da631868416377
Jose Carvajal <joe_carvajal@hotmail.com>
2015-10-05 23:31:02 -0500

    Limpiando codigo de quiz 3

--- laboratorios/quiz3.py
+++ laboratorios/quiz3.py
@@ -3,17 +3,14 @@
 # restantes que corresponden para convertirse exactamente en minutos.
 # Este programa debe funcionar para 5 oportunidades
 
-intentos = 5
 x = 1
-while x <= intentos:
+while x <= 5:
 
 	tiempo = int (input("Introduzca un intervalo de tiempo en segundos: "))
 	aux = 60
-	n = 1
 	
 	while aux <= tiempo:
 		aux += 60
-		n += 1
 		
 	t_restante = aux - tiempo
 	print("Los segundos restantes para llegar al proximo minuto son: " +str(t_restante))
