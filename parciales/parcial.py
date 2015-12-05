607bae3d0fdc4da1cd202e7c30ff0de776f96626
Jose Carvajal <joe_carvajal@hotmail.com>
2015-12-05 04:05:31 -0500

    Subiendo Parcial

---
+++ parciales/parcial.py
@@ -0,0 +1,54 @@
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+import random
+
+pregResp = [ ("Panama tiene mas de cien distritos.", 0), ("Panama cuenta con 5 comarcas indigenas.", 1), 
+			("Presidente actual: Ricardo Martinelli", 0), ("Fundacion de la ciudad de Panama: 15 de agosto de 1519.", 1),
+			("Panama tiene frontera con Colombia y Nicaragua.", 0) ]
+			
+
+pts = 0
+i = 0
+
+def sumarPts(resp):
+	global pts
+	global i
+	if resp == pregResp[i][1]:
+	
+		print("correcto")
+		pts += 10 
+		print("total de puntos: " + str(pts))
+		#print(i)
+		#probando. La i no se esta actualizando
+	else:
+		print("equivocado")
+		print("total de puntos: " + str(pts))
+		#print(i)
+		#probando. La i no se esta actualizando
+		
+class YourWidget(Widget):
+	pregunta = StringProperty()
+
+	def __init__(self, **kwargs):
+		global i
+		i = random.randint(0, 4)
+		super(YourWidget, self).__init__(**kwargs)
+		self.pregunta = str(pregResp[i][0])
+
+	def change_text(self):
+		global i
+		i = random.randint(0, 4)
+		#print (i)
+		#probando. La i no se esta actualizando en la funcion sumarPts
+		self.pregunta = str(pregResp[i][0])
+
+class YourApp(App):
+	def build(self):
+		return YourWidget()
+	
+	
+
+if __name__ == '__main__':
+	YourApp().run()
